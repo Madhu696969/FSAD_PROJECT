85e2f55c3147d1612339a29a3be3b47689bd7cab
Madhu696969 <2400030376@kluniversity.in>
2026-03-19 07:39:29 +0530

    Modified Profile

--- src/Components/Donor/DonorProfile.jsx
+++ src/Components/Donor/DonorProfile.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import DonorNavbar from '../../NavBar/DonorNavbar';
 
 const DonorProfile = () => {
+    const donor=JSON.parse(localStorage.getItem("donor"));
   const navigate = useNavigate();
     const handleLogout = () => {
         localStorage.removeItem("donor");
@@ -10,7 +12,18 @@ const DonorProfile = () => {
     }
     return (
         <div>
-            <button onClick={handleLogout}>Logout</button>
+            <DonorNavbar/>
+            <div className="card1">
+                <div className="box">
+                    <img style={{ width: "150px", height: "150px" }} src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001877.png" alt="" />
+                    <div className="p-Cont">
+                        <p>Name :- {donor.name}</p>
+                        <p>email :- {donor.email}</p>
+                        <p>PhoneNo :- {donor.phoneNumber}</p>
+                    </div>
+                </div>
+                <button style={{marginLeft:"290px"}} onClick={handleLogout}>Logout</button>
+            </div>
         </div>
     )
 }
